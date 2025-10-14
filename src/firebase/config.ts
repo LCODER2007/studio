a76cdb3a87da11c3088013675bc9fc87f129af5a
Lawal Giyath <lawalgiyath200716@gmail.com>
2025-10-14 07:57:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2819594368-36cb9",
+  "appId": "1:110620089235:web:1ecf600b42c279b5aca666",
+  "apiKey": "AIzaSyCuZq7jm4lMIFq-E6SxjpmkvkjEKMaphxM",
+  "authDomain": "studio-2819594368-36cb9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110620089235"
+};
